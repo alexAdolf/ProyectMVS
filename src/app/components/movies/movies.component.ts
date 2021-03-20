e3103924e344359292f3f87e9ec35a665f7bbd62
Adolfo <adolfo.astorga@clave.cl>
2021-03-20 15:23:22 -0300

    uso de modelo para cargar peliculas

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
+import { FilmsModule } from '../../films/films.module';
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
@@ -8,14 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class MoviesComponent implements OnInit {
 
   public title: string
-  public peliculas: Array<any>;
+  public peliculas: FilmsModule[];
 
   constructor() {
     this.title = "Componente Peliculas";
     this.peliculas = [
-      { year: 2019, title: "Spiderman", image: 'https://dam.smashmexico.com.mx/wp-content/uploads/2019/06/spider-man-far-from-home-fin-mcu-fase-3-razon-kevin-feige-cover.jpg', },
-      { year: 2020, title: "Avengers", image: 'https://cnet1.cbsistatic.com/img/l-xJp5JmvfZUGTVlfJ7O-wVVRTI=/940x0/2019/03/14/70b49c1d-0d3b-4b75-9225-b898b83cdc9a/avengers-endgame-poster-og-social-crop.jpg', },
-      { year: 2021, title: "Batman", image: 'https://media.gq.com.mx/photos/5f8336a73515c53dd2a8653c/master/pass/the-batman-robert-pattinson-matt-reeves.jpg', }
+      new FilmsModule("Spiderman", 2019, "https://dam.smashmexico.com.mx/wp-content/uploads/2019/06/spider-man-far-from-home-fin-mcu-fase-3-razon-kevin-feige-cover.jpg"),
+      new FilmsModule("Avenger", 2020, "https://cnet1.cbsistatic.com/img/l-xJp5JmvfZUGTVlfJ7O-wVVRTI=/940x0/2019/03/14/70b49c1d-0d3b-4b75-9225-b898b83cdc9a/avengers-endgame-poster-og-social-crop.jpg"),
+      new FilmsModule("Batman", 2021, "https://media.gq.com.mx/photos/5f8336a73515c53dd2a8653c/master/pass/the-batman-robert-pattinson-matt-reeves.jpg"),
+      // { year: 2019, title: "Spiderman", image: 'https://dam.smashmexico.com.mx/wp-content/uploads/2019/06/spider-man-far-from-home-fin-mcu-fase-3-razon-kevin-feige-cover.jpg', },
+      // { year: 2020, title: "Avengers", image: 'https://cnet1.cbsistatic.com/img/l-xJp5JmvfZUGTVlfJ7O-wVVRTI=/940x0/2019/03/14/70b49c1d-0d3b-4b75-9225-b898b83cdc9a/avengers-endgame-poster-og-social-crop.jpg', },
+      // { year: 2021, title: "Batman", image: 'https://media.gq.com.mx/photos/5f8336a73515c53dd2a8653c/master/pass/the-batman-robert-pattinson-matt-reeves.jpg', }
     ]
    }
 
